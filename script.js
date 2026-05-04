e090d686d6394d0a59bf1ac0b4757cace4c81a76
Ahmed Bilal <ahmed258bilal@gmail.com>
2026-05-04 12:29:44 +0500

    fixing calculation issues and verify the values before calculating BMI

--- script.js
+++ script.js
@@ -5,12 +5,22 @@ let age = document.getElementById("age");
 let height = document.getElementById("height");
 let weight = document.getElementById("weight");
 
-let BMI = weight / (height * height);
 function cal() {
+  let w = Number(weight.value);
+  let h = Number(height.value);
+  if (h <= 0 || w <= 0) {
+    alert("enter a valid value of height and weight.");
+  }
+  // converting height into meters
+  h = h / 100;
+  let BMI = w / (h * h);
+
+  let result = BMI.toFixed(2);
+  console.log(typeof BMI);
   console.log(username.value);
   console.log(gender.value);
   console.log(age.value);
   console.log(height.value);
   console.log(weight.value);
-  console.log("BMI value=", BM);
+  console.log("BMI value=", result);
 }
